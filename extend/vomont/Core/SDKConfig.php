156f0688e8538d98d79540d351d21fe299f67dc2
caijunqi99 <xiaojianhai.git>
2019-07-01 17:45:23 +0800

    添加

---
+++ extend/vomont/Core/SDKConfig.php
@@ -0,0 +1,14 @@
+<?php
+namespace vomont\Core;
+
+/**
+ * 物盟登陆信息
+ */
+class SDKConfig
+{
+
+	const HTTP_URL       = 'http://117.78.26.155:8050/?';
+	const KEY            = 'webuser';
+	const VomontUsername = 'test';
+	const VomontPswd     = 'e10adc3949ba59ab';
+}
